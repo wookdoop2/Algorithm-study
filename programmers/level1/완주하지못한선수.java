803aeb16c2c98b3375324da0456380c9d3021648
sungwook <steins3391@naver.com>
2020-09-02 10:13:52 +0900

    완주하지못한선수 #-

--- programmers/level1/완주하지못한선수.java
+++ programmers/level1/완주하지못한선수.java
@@ -1,15 +1,17 @@
 package programmers.level1;
 
+import java.util.*;
+
 public class 완주하지못한선수 {
     public static String solution(String[] participant, String[] completion) {
-        String answer = "";
-        for(int i = 0; i < participant.length; i++) {
-            for(int j = 0; j < completion.length; j++) {
-                if(participant[i].equals(completion[j])) {
-                    System.out.println(participant[i]);
-                }
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for(i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                return participant[i];
             }
         }
-        return answer;
+        return participant[i];
     }
 }
