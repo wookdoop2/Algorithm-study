59aa73fd9502f2b30970cad836a063a65e0345a7
sungwook <steins3391@naver.com>
2020-09-18 21:59:54 +0900

    요세푸스문제 #2

--- baekjoon/알고리즘기초1/자료구조1/요세푸스문제.py
+++ baekjoon/알고리즘기초1/자료구조1/요세푸스문제.py
@@ -2,5 +2,14 @@ count, position = input().split()
 count = int(count)
 position = int(position)
 
-for i in range(0, count):
-    print(count)
+answer = "<"
+_list_ = list(range(1, count + 1))
+
+while 1:
+    if len(_list_) == 1:
+        answer += str(_list_.pop(0)) + ">"
+        break
+    for i in range(0, position - 1):
+        _list_.append(_list_.pop(0))
+    answer += str(_list_.pop(0)) + ", "
+print(answer)
