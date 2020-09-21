8f29f1828aebb88d89947acf8343ccf34010ae57
sungwook <steins3391@naver.com>
2020-09-21 19:35:32 +0900

    덱 #-

--- baekjoon/알고리즘기초1/자료구조1/덱.py
+++ baekjoon/알고리즘기초1/자료구조1/덱.py
@@ -1,27 +1,45 @@
 from collections import deque
+import sys
 
-count = int(input())
+count = int(sys.stdin.readline())
 
 dq = deque()
 
+
+def empty():
+    if len(dq) == 0:
+        return 1
+    else:
+        return 0
+
+
 for i in range(count):
-    msg = list(input().split())
+    msg = list(sys.stdin.readline().split())
     if msg[0] == "push_back":
-        dq.appendleft(msg[1])
-    elif msg[0] == "push_front":
         dq.append(msg[1])
+    elif msg[0] == "push_front":
+        dq.appendleft(msg[1])
     elif msg[0] == "pop_back":
-        dq.popleft()
+        if empty() == 1:
+            print("-1")
+        else:
+            print(dq.pop())
     elif msg[0] == "pop_front":
-        dq.pop()
+        if empty() == 1:
+            print("-1")
+        else:
+            print(dq.popleft())
     elif msg[0] == "back":
-        print(dq[0])
+        if empty() == 1:
+            print(-1)
+        else:
+            print(dq[len(dq) - 1])
     elif msg[0] == "front":
-        print(dq[len(dq) - 1])
+        if empty() == 1:
+            print("-1")
+        else:
+            print(dq[0])
     elif msg[0] == "size":
         print(len(dq))
     elif msg[0] == "empty":
-        if len(dq) == 0:
-            print(1)
-        else:
-            print(0)
+        print(empty())
