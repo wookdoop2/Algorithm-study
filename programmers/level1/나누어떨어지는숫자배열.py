819c42fa7c4edb4541291588f45333b2e6cd9f9c
sungwook <steins3391@naver.com>
2020-10-03 18:59:32 +0900

    나누어떨어지는숫자배열 #-

---
+++ programmers/level1/나누어떨어지는숫자배열.py
@@ -0,0 +1,13 @@
+def solution(arr, divisor):
+    answer = []
+    new_list = []
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+
+    if len(answer) == 0:
+        answer.append(-1)
+        return answer
+    answer.sort()
+    return answer
+ㄴ
