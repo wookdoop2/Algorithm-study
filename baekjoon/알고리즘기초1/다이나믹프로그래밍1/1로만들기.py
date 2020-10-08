66eda5fd6536c99f12610186fa648aeece60a4f7
sungwook <steins3391@naver.com>
2020-10-08 17:04:31 +0900

    1로만들기 #-

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/1로만들기.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/1로만들기.py
@@ -24,3 +24,13 @@ while 1:
     if min(min_num) == 1:
         print(answer)
         break
+
+# Bottom-Up
+# num_list = [0] * (num + 1)
+# for i in range(2, num + 1):
+#     num_list[i] = num_list[i - 1] + 1
+#     if i % 2 == 0 and num_list[i] > num_list[i // 2] + 1:
+#         num_list[i] = num_list[i // 2] + 1
+#     if i % 3 == 0 and num_list[i] > num_list[i // 3] + 1:
+#         num_list[i] = num_list[i // 3] + 1
+# print(num_list[num])
