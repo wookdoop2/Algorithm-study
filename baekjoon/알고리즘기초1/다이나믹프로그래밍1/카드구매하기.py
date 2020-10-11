d2f522da09b6d073642fd3b812d8025169206f0c
sungwook <steins3391@naver.com>
2020-10-11 19:03:09 +0900

    카드구매하기 #-

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/카드구매하기.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/카드구매하기.py
@@ -3,9 +3,8 @@ N = int(input())
 cost = list(map(int, input().split()))
 
 _list_ = [0] * (N + 1)
-_list_[1] = 1
 
-for i in range(2, N + 1):
+for i in range(1, N + 1):
     for j in range(1, i + 1):
         _list_[i] = max(_list_[i], cost[j - 1] + _list_[i - j])
 print(_list_[N])
