fd75157eb749c6337d50a8ebb61b073e9cbe3a34
sungwook <steins3391@naver.com>
2020-10-17 23:47:03 +0900

    N으로표현 #-

--- programmers/level3/N으로표현.py
+++ programmers/level3/N으로표현.py
@@ -3,19 +3,16 @@
 def solution(N, number):
     _set_ = [set() for i in range(9)]
     _set_[0].add(0)
-    _set_[1].add(N)
-    for i in range(2, 9):
+    for i in range(1, 9):
         _set_[i].add(int(str(N) * i))
         for j in range(1, i):
             for k in _set_[j]:
                 for m in _set_[i - j]:
-                    _set_[i].add(int(k + m))
-                    _set_[i].add(int(k - m))
-                    _set_[i].add(int(k * m))
-                    if k == 0:
-                        continue
+                    _set_[i].add(k + m)
+                    _set_[i].add(k - m)
+                    _set_[i].add(k * m)
                     if m != 0:
-                        _set_[i].add(int(k / m))
+                        _set_[i].add(k // m)
         if number in _set_[i]:
             return i
     return -1
