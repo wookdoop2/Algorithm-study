b12694291799b58d27eeaf598978bb166e0ad47d
sungwook <steins3391@naver.com>
2020-10-26 23:30:18 +0900

    제곱수의합 #-

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/제곱수의합.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/제곱수의합.py
@@ -1,19 +1,14 @@
 # Baekjoon Online Judge
 # Dynamic Programming
-max_num = 100000
+max_num = int(input())
 _list_ = [0] * (max_num + 1)
 
-_list_[1] = 1
-
-num = 1
-
-for i in range(1, 12):
-    if i == ((num + 1) ** 2):
+for i in range(1, max_num + 1):
+    num = 1
+    _list_[i] = i
+    while num * num <= i:
+        if _list_[i] > _list_[i - (num * num)] + 1:
+            _list_[i] = _list_[i - (num * num)] + 1
         num += 1
-        _list_[i] = 1
-    else:
-        _list_[i] = min(_list_[i - 1] + 1, 1 + _list_[i - (num ** 2)])
-    print(_list_)
 
-n = int(input())
-print(_list_[n])
+print(_list_[max_num])
