7d96fd85313ab57219eee746c0d8ec94ece1430e
sungwook <steins3391@naver.com>
2020-10-27 21:17:34 +0900

    합분해 #-

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/합분해.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/합분해.py
@@ -1,9 +1,18 @@
+# Baekjoon Online Judge
+# Dynamic Programming
 num, count = map(int, input().split())
+
+mod_num = 1000000000
 
 _list_ = [[0] * (count + 1) for _ in range(num + 1)]
 
-for i in range(1, num + 1):
-
-    # logic
+for i in range(0, num + 1):
     for j in range(1, count + 1):
-        _list_[i][j] = _list_[i][j - 1] + 1
+        if j == 1:
+            _list_[i][j] = 1
+            continue
+        for k in range(0, i + 1):
+            _list_[i][j] += _list_[i - k][j - 1]
+        _list_[i][j] %= mod_num
+
+print(_list_[num][count])
