b4e041398004bbe5e68f1ef406745481c28efa9b
sungwook <steins3391@naver.com>
2020-11-02 22:14:29 +0900

    스티커 #2

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/스티커.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/스티커.py
@@ -9,8 +9,9 @@ for i in range(n):
     _list_ = [[0] * 3 for _ in range(count)]
     for j in range(2):
         cost_list.append(list(map(int, input().split())))
-    _list_[0][1] = cost_list[0][0]
-    _list_[0][2] = cost_list[1][0]
+    # print(cost_list)
+    _list_[1][0] = cost_list[0][0]
+    _list_[2][0] = cost_list[1][0]
     for j in range(1, count):
         _list_[0][j] = max(_list_[1][j - 1], _list_[2][j - 1])
         _list_[1][j] = max(_list_[0][j - 1] + cost_list[0][j], _list_[2][j - 1] + cost_list[0][j])
