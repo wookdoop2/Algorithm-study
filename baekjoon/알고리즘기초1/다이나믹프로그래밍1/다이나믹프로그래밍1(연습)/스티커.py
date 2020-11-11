72743e742ce8c00944ea4fb97d2527bfe037fa08
sungwook <steins3391@naver.com>
2020-11-11 22:58:21 +0900

    스티커 #-

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/스티커.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/스티커.py
@@ -10,10 +10,10 @@ for i in range(n):
     for j in range(2):
         cost_list.append(list(map(int, input().split())))
     # print(cost_list)
-    _list_[1][0] = cost_list[0][0]
-    _list_[2][0] = cost_list[1][0]
+    _list_[0][1] = cost_list[0][0]
+    _list_[0][2] = cost_list[1][0]
     for j in range(1, count):
-        _list_[0][j] = max(_list_[1][j - 1], _list_[2][j - 1])
-        _list_[1][j] = max(_list_[0][j - 1] + cost_list[0][j], _list_[2][j - 1] + cost_list[0][j])
-        _list_[2][j] = max(_list_[0][j - 1] + cost_list[1][j], _list_[1][j - 1] + cost_list[1][j])
-    print(_list_[count - 1])
+        _list_[j][0] = max(_list_[j - 1][1], _list_[j - 1][2])
+        _list_[j][1] = max(_list_[j - 1][0] + cost_list[0][j], _list_[j - 1][2] + cost_list[0][j])
+        _list_[j][2] = max(_list_[j - 1][0] + cost_list[1][j], _list_[j - 1][1] + cost_list[1][j])
+    print(max(_list_[count - 1]))
