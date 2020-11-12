e3c1df79411e695a3eb7720515dc3b8c5b625ed4
sungwook <steins3391@naver.com>
2020-11-12 23:45:08 +0900

    포도주시식 #1

---
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/포도주시식.py
@@ -0,0 +1,9 @@
+# Baekjoon Online Judge
+# Dynamic Programming
+# https://www.acmicpc.net/problem/2156
+n = int(input())
+
+_list_ = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    _list_[i] = int(input())
