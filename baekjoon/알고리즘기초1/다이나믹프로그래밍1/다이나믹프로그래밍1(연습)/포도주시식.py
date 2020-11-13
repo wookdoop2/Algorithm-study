06cad60bc1dc781c6eec0a30ca78f356a8fd6136
sungwook <steins3391@naver.com>
2020-11-13 23:04:34 +0900

    포도주시식 #2

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/포도주시식.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/포도주시식.py
@@ -3,7 +3,14 @@
 # https://www.acmicpc.net/problem/2156
 n = int(input())
 
-_list_ = [0] * (n + 1)
+_list_ = [[0] * 3 for _ in range(n)]
 
-for i in range(1, n + 1):
-    _list_[i] = int(input())
+for i in range(n):
+    cost = int(input())
+    if i == 0:
+        _list_[i][1] = _list_[i - 1][0] + cost
+    _list_[i][0] = max(_list_[i - 1][0], _list_[i - 1][1], _list_[i - 1][2])
+    _list_[i][1] = _list_[i - 1][0] + cost
+    _list_[i][2] = _list_[i - 1][1] + cost
+
+print(max(_list_[n - 1]))
