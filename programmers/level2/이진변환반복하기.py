b34a7bae5395ebfbe95fa1e268f79d2f323c7a57
sungwook <steins3391@naver.com>
2020-11-21 23:52:21 +0900

    이진변환반복하기 #-

--- programmers/level2/이진변환반복하기.py
+++ programmers/level2/이진변환반복하기.py
@@ -3,41 +3,25 @@
 # https://programmers.co.kr/learn/courses/30/lessons/70129
 # 월간 코드 챌린지 시즌1
 def decimal_to_binary(num):
-    binary_num = []
-    while True:
-        if num % 2 == 0:
-            binary_num.insert(0, '0')
-        else:
-            binary_num.insert(0, '1')
-
-        num = num // 2
-
-        if num == 1:
-            binary_num.insert(0, '1')
-            break
-        elif num == 0:
-            binary_num.insert(0, '0')
-            break
-
-    return binary_num
+    if num == 1:
+        return '1'
+    return decimal_to_binary(num // 2) + str(num % 2)
 
 
 def solution(s):
-    answer = []
     count = 0
     count_zero = 0
 
-    while True:
-        _list_ = []
-        s = list(map(int, s))
+    while s != '1':
+        count_one = 0
+        s = list(s)
         for i in s:
-            if i == 1:
-                _list_.append(i)
+            if i == '1':
+                count_one += 1
             else:
                 count_zero += 1
-        s = decimal_to_binary(len(_list_))
+        s = decimal_to_binary(count_one)
 
-        if int(''.join(s)) == '10':
-            break
         count += 1
+
     return count, count_zero
