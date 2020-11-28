53870f817c7fab882625b3348ade158761851ef2
sungwook <steins3391@naver.com>
2020-11-28 22:11:25 +0900

    가장큰증가부분수열 #- !

--- baekjoon/알고리즘분류/브루트포스/리모컨.py
+++ baekjoon/알고리즘분류/브루트포스/리모컨.py
@@ -2,19 +2,22 @@
 # Brute Force
 # https://www.acmicpc.net/problem/1107
 # 2020 NHN godo 하반기 공채
-page = int(input())
-count = int(input())
-broken = list(map(int, input().split()))
-
-max_page = 500000
-
 buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 not_broken = []
 
-# Not broken buttons
-for i in buttons:
-    if i not in broken:
-        not_broken.append(i)
+page = int(input())
+count = int(input())
+if count == 0:
+    not_broken = buttons
+else:
+    broken = list(map(int, input().split()))
+
+    # Not broken buttons
+    for i in buttons:
+        if i not in broken:
+            not_broken.append(i)
+
+max_page = 500000
 
 answer = abs(page - 100)
 
