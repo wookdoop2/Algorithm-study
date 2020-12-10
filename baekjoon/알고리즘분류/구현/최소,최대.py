9dd6fd85579006acb490abcb02dbf36f05575f56
sungwook <steins3391@naver.com>
2020-12-10 20:08:35 +0900

    최소,최대 #-

---
+++ baekjoon/알고리즘분류/구현/최소,최대.py
@@ -0,0 +1,18 @@
+# Baekjoon Online Judge
+# Simulation
+# https://www.acmicpc.net/problem/1107
+count = int(input())
+_list_ = list(map(int, input().split()))
+
+min_num = 1000000
+max_num = -1000000
+
+for i in _list_:
+
+    if min_num > i:
+        min_num = i
+
+    if max_num < i:
+        max_num = i
+
+print(min_num, max_num)
