743e42976a2f7877901b5f03a1c87c81f52977d8
sungwook <steins3391@naver.com>
2020-12-27 17:23:15 +0900

    풍선터트리기 #-

--- programmers/level3/풍선터트리기.py
+++ programmers/level3/풍선터트리기.py
@@ -8,20 +8,36 @@ def solution(a):
     # 번호가 더 작은 풍선을 2번 터트려야함 ->
     # 무조건 마지막까지 남을 수 없음
     _list_ = []
+    min_left = [0] * len(a)
+    min_right = [0] * len(a)
+
+    min_num = a[0]
+    for i in range(1, len(a)):
+        min_left[i] = min_num
+
+        if a[i] < min_num:
+            min_num = a[i]
+
+    min_num = a[-1]
+    for i in range(len(a) - 2, 0, -1):
+        min_right[i] = min_num
+
+        if a[i] < min_num:
+            min_num = a[i]
+
+    # print(min_left)
+    # print(min_right)
+
     for i in range(len(a)):
-        if i == 0:
-            # right_min = min(a[1:])
-            _list_.append(a[i])
-        elif i == len(a) - 1:
-            # left_min = min(a[:i])
+        if i == 0 or i == len(a) - 1:
             _list_.append(a[i])
         else:
-            left_min = min(a[:i])
-            right_min = min(a[i + 1:])
-
-            if a[i] > left_min and a[i] > right_min:
+            if a[i] > min_left[i] and a[i] > min_right[i]:
                 continue
             else:
                 _list_.append(a[i])
+
+    # print(_list_)
     answer = len(_list_)
+
     return answer
