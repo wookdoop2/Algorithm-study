6dc8d296690586a24de7528cee2fcefdce263603
sungwook <steins3391@naver.com>
2021-03-08 00:43:50 +0900

    숨바꼭질 #-

--- baekjoon/알고리즘기초1/BFS/숨바꼭질.py
+++ baekjoon/알고리즘기초1/BFS/숨바꼭질.py
@@ -1,28 +1,36 @@
 # Baekjoon Online Judge
 # BFS
 # https://www.acmicpc.net/problem/1697
-start, end = input().split()
-max_num = 100000
-check = [0] * max_num
+start, end = map(int, input().split())
+max_num = 200000
+check = [0] * (max_num + 1)
+count = [0] * (max_num + 1)
 _list_ = []
 
 check[start] = 1
 _list_.append(start)
 
-while len(_list_) !=0:
-    now = _list_[-1]
+while len(_list_) != 0:
 
-    if now - 1 >= 0:
-        if check[now - 1] == 0:
-            _list_.append(now - 1)
-            check[now - 1] = 1
+    now = _list_.pop(0)
 
-    if now + 1 <= max_num:
-        if check[now + 1] == 0:
-            _list_.append(now + 1)
-            check[now + 1] = 1
+    if 0 <= now - 1 <= max_num and check[now - 1] == 0:
+        _list_.append(now - 1)
+        count[now - 1] = count[now] + 1
+        check[now - 1] = 1
 
-    if now * 2 <= max_num:
-        if check[now * 2] == 0:
-            _list_.append(now * 2)
-            check[now * 2] = 1
+    if 0 <= now + 1 <= max_num and check[now + 1] == 0:
+        _list_.append(now + 1)
+        count[now + 1] = count[now] + 1
+        check[now + 1] = 1
+
+    if 0 <= now * 2 <= max_num and check[now * 2] == 0:
+        _list_.append(now * 2)
+        count[now * 2] = count[now] + 1
+        check[now * 2] = 1
+
+    # 시간 초과
+    # if end in _list_:
+    #     break
+
+print(count[end])
