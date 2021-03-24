263c2b99c55742279a42c3d408dc74fb60639bbd
sungwook <steins3391@naver.com>
2021-03-24 17:13:07 +0900

    주사위네개 #-

---
+++ baekjoon/알고리즘분류/구현/주사위네개.py
@@ -0,0 +1,19 @@
+# Baekjoon Online Judge
+# Implementation
+# https://www.acmicpc.net/problem/1748
+num = input()
+
+length, answer = len(num), 0
+
+# 1 ~ 9
+# 10 ~ 99
+# 100 ~ 999
+# 1000 ~ 9999
+# ...
+for l in range(1, length):
+    answer += l * (10 ** l - 10 ** (l - 1))
+
+# 10 ** (length - 1) ~ num
+answer += length * (int(num) - 10 ** (length - 1) + 1)
+
+print(answer)
