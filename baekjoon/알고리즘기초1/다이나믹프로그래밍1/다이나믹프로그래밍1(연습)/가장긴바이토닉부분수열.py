b79832647e2c7ae6c20b1d270327f00c4f72833f
sungwook <steins3391@naver.com>
2021-03-24 17:52:37 +0900

    가장긴바이토닉부분수열 #-

--- baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/가장긴바이토닉부분수열.py
+++ baekjoon/알고리즘기초1/다이나믹프로그래밍1/다이나믹프로그래밍1(연습)/가장긴바이토닉부분수열.py
@@ -5,6 +5,7 @@ n = int(input())
 
 cost = list(map(int, input().split()))
 
+# case1 - 가장 긴 증가하는 부분 수열
 up_list = [0] * n
 up_list[0] = 1
 
@@ -14,18 +15,24 @@ for i in range(1, n):
         if cost[j] < cost[i] and up_list[i] < up_list[j] + 1:
             up_list[i] = up_list[j] + 1
 
-print(up_list)
+# print(up_list)
 
-down_list = [0] * n
-down_list[0] = 1
+# case2 - 가장 긴 증가하는 부분 수열 (거꾸로)
+up_reversed_list = [0] * n
+up_reversed_list[n - 1] = 1
 
-for i in range(1, n):
-    down_list[i] = 1
-    for j in range(i):
-        if cost[j] > cost[i] and down_list[i] < down_list[j] + 1:
-            down_list[i] = down_list[j] + 1
+for i in range(n - 2, -1, -1):
+    up_reversed_list[i] = 1
+    for j in range(n - 1, i, -1):
+        if cost[j] < cost[i] and up_reversed_list[i] < up_reversed_list[j] + 1:
+            up_reversed_list[i] = up_reversed_list[j] + 1
 
-print(down_list)
+# print(up_reversed_list)
 
-d = [up_list[i] + down_list[i] - 1 for i in range(n)]
-print(max(d))
+# 두 case를 더해서 가장 값이 큰 곳 -> 가장 긴 바이토닉 부분 수열
+max_num = 0
+for i in range(n):
+    if max_num < up_list[i] + up_reversed_list[i]:
+        max_num = up_list[i] + up_reversed_list[i]
+
+print(max_num - 1)
